2bd61eb16d224140d65d07d617b459ec43c8c833
shift <13461239+choby@users.noreply.github.com>
2020-03-23 16:52:20 +0700

    Update RequiredKeys.ts

--- RequiredKeys.ts
+++ RequiredKeys.ts
@@ -1,3 +1,14 @@
 import { OptionalKeys } from "./OptionalKeys";
 
 export type RequiredKeys<T> = Exclude<keyof T,OptionalKeys<T>>;
+
+
+// interface T1 {
+//     a: string;
+//     b?: string;
+//     c: string;
+//     d?: string;
+// }
+// //test:
+// type T4 = RequiredKeys<T1>;
+// T4: "a" | "c" 
