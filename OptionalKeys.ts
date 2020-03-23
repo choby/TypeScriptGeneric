62c948b69c09060433829bcdc586e79a0e6cdab5
shift <13461239+choby@users.noreply.github.com>
2020-03-23 16:48:58 +0700

    Update OptionalKeys.ts

--- OptionalKeys.ts
+++ OptionalKeys.ts
@@ -4,7 +4,14 @@
 // }[keyof T];
 
 // //test:
+// interface T1 {
+//     a: string;
+//     b?: string;
+//     c: string;
+//     d?: string;
+// }
 // type T2 = OptionalKeys<T1>;
+// T2: "b"|"d"
 
 // 未开启严格null检查的项目可以实用以下定义
 type IsOptional<T, K extends keyof T> = { [K1 in Exclude<keyof T, K>]: T[K1] } & { K?: T[K] } extends T ? K : never;
@@ -12,6 +19,7 @@ export type OptionalKeys<T> = { [K in keyof T]-?: IsOptional<T, K> }[keyof T];
 
 // //test:
 // type T3 = OptionalKeys<T1>;
+// T3: "b"|"d"
 
 
 
